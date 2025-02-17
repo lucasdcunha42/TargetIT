614af1a684c39295cc1fcab11b2a74825669d1b1
lucasdcunha42 <lucasdcunha42>
2025-02-17 03:49:06 -0300

    feat: enhance UserFactory to include phone and CPF fields, update password, and add admin role state

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name'              => $this->faker->name,
+            'email'             => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password'          => Hash::make('secret123'),
+            'remember_token'    => Str::random(10),
+            'phone'             => $this->faker->phoneNumber,
+            'cpf'               => $this->faker->numerify('###.###.###-##'),
         ];
     }
 
@@ -40,5 +42,14 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    public function admin()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+            'role' => 'admin',
+            ];
+        });
     }
 }
